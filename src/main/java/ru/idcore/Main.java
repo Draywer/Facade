318363ac31d0519568b6fa75fa06782b949c7691
Alexander Gnatenko <agnatenko@gmail.com>
2021-02-07 21:47:52 +0300

    Решение задачи

---
+++ src/main/java/ru/idcore/Main.java
@@ -0,0 +1,9 @@
+package ru.idcore;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+        System.out.println(binOps.sum("00000010","00000011"));
+        System.out.println(binOps.mult("00000010","00000011"));
+    }
+}
